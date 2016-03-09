b81839edd14038822aa70813480c5b38cbfba0e9
arktisklada <mail@enorganik.com>
2016-03-08 21:32:47 -0600

    Massive cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,5 @@
-$(function () {
-  $("span.menu").click(function(){
-    $(".top-menu ul").slideToggle("slow" , function(){
-    });
-  });
-
-  $("#slider4").responsiveSlides({
-    auto: true,
-    pager: true,
-    nav: true,
-    speed: 500,
-    namespace: "callbacks",
-    before: function () {
-      // $('.events').append("<li>before event fired.</li>");
-    },
-    after: function () {
-      // $('.events').append("<li>after event fired.</li>");
-    }
-  });
-
-  $(".scroll").click(function(event){    
-    event.preventDefault();
-    $('html, body').animate({scrollTop:$(this.hash).offset().top},1000);
+$(function() {
+  $("span.menu").click(function() {
+    $(".top-menu ul").slideToggle("fast");
   });
 });
