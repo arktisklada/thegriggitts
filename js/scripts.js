3e4e7530f5267e045b1f245d688c2853bc56bdeb
arktisklada <mail@enorganik.com>
2016-03-11 03:38:17 -0600

    Bridal party

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,32 @@ $(function() {
   $("span.menu").click(function() {
     $(".top-menu ul").slideToggle("fast");
   });
+
+  $(".people-images").each(function() {
+    function hoverEvent($peopleImages, $target, elements) {
+      $peopleImages.find("img").hover(function() {
+        $image = $(this);
+        $content = $image.siblings(".person-content");
+        $target.removeClass("empty");
+        for (var key in elements) {
+          elements[key].html($content.children(key).html());
+        }
+      }, function() {
+        for (var key in elements) {
+          elements[key].html("");
+        }
+        $target.addClass("empty");
+      });
+    }
+
+    $peopleImages = $(this);
+    $target = $($peopleImages.data("target"));
+    elements = {
+      h4: $target.find("h4"),
+      h6: $target.find("h6"),
+      p: $target.find("p")
+    }
+
+    hoverEvent($peopleImages, $target, elements);
+  });
 });
